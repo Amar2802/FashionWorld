f769bb422446af88b8999817be7187f797acf692
Amarnath Yadav <Amar2822@outlook.com>
2026-04-23 15:38:43 +0530

    fix cors + final backend setup

--- server/index.js
+++ server/index.js
@@ -10,10 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "https://fashion-world-zeta.vercel.app/",
+  origin: [
+    "http://localhost:5173",
+    "https://fashion-world-zeta.vercel.app"
+  ],
   credentials: true
 }));
-
 
 app.use(express.json());
 
